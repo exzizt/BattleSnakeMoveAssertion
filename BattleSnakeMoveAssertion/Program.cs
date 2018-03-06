c9cb13a4f3a40e070ecefb0db23968d4f7a5eef6
Luke <lukegamitchell@gmail.com>
2018-03-05 23:40:36 -0800

    Update

--- BattleSnakeMoveAssertion/Program.cs
+++ BattleSnakeMoveAssertion/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace BattleSnakeMoveAssertion
 {
@@ -48,6 +48,7 @@ namespace BattleSnakeMoveAssertion
                 if (!(JsonConvert.DeserializeObject(worldJson, typeof(Messages.World)) is Messages.World))
                 {
                     Console.WriteLine(moveAssertionFile + ": contains invalid World JSON. Please review the API.");
+                    return;
                 }
 
                 // Get the snake's move for this world:
